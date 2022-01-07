f86d4590304ea7ebc384741084c8afd79e092d04
Martin Ballester <martinballester1@gmail.com>
2022-01-07 15:42:58 -0300

    add search

--- app/src/main/java/com/martin/preventapp/ui/new_order/NewOrderFragment.java
+++ app/src/main/java/com/martin/preventapp/ui/new_order/NewOrderFragment.java
@@ -27,13 +27,6 @@ public class NewOrderFragment extends Fragment {
         binding = FragmentNewOrderBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNewOrder;
-        newOrderViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 

8fecdd765d90bb0965131c4768c3e89b609d284e
Martin Ballester <martinballester1@gmail.com>
2022-01-08 18:13:26 -0300

    connect to firebase

---
+++ app/src/main/java/com/martin/preventapp/firebase/Clients.java
@@ -0,0 +1,33 @@
+package com.martin.preventapp.firebase;
+
+import java.util.ArrayList;
+
+public class Clients {
+
+    public ArrayList<String> clientlist ()
+    {
+        ArrayList<String> numberList = new ArrayList<>();
+
+        numberList.add("+ Agregar un cliente");
+        numberList.add("One");
+        numberList.add("Three");
+        numberList.add("Four");
+        numberList.add("Five");
+        numberList.add("Six");
+        numberList.add("Seven");
+        numberList.add("Eight");
+        numberList.add("Nine");
+        numberList.add("Ten");
+        numberList.add("Three");
+        numberList.add("Four");
+        numberList.add("Five");
+        numberList.add("Six");
+        numberList.add("Seven");
+        numberList.add("Eight");
+        numberList.add("Nine");
+        numberList.add("Ten");
+
+
+        return numberList;
+    }
+}
--- app/src/main/java/com/martin/preventapp/ui/new_order/NewOrderFragment.java
+++ app/src/main/java/com/martin/preventapp/ui/new_order/NewOrderFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,7 +15,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.martin.preventapp.R;
 import com.martin.preventapp.databinding.FragmentNewOrderBinding;
+import com.martin.preventapp.firebase.Clients;
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class NewOrderFragment extends Fragment {
 
@@ -26,6 +41,52 @@ public class NewOrderFragment extends Fragment {
 
         binding = FragmentNewOrderBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        SearchableSpinner spinner = root.findViewById(R.id.spinner_searchable_new_order);
+        TextView text = root.findViewById(R.id.textView3);
+
+        Clients client = new Clients();
+
+        String PATH_START = "start";
+        String PATH_MESSAGE = "message";
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference reference = database.getReference(PATH_START).child(PATH_MESSAGE);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                text.setText(snapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                text.setText("Error al consultar en firebase");
+            }
+        });
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0)
+                {
+                    text.setText("Seleccione el cliente");
+                }
+                else
+                {
+                    String sNumber = adapterView.getItemAtPosition(i).toString();
+                    text.setText(sNumber);
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         return root;
     }

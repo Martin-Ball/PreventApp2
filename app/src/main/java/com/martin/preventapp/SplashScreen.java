05dee5a842878ab9b0eaf7051f31ae036483b378
Martin Ballester <martinballester1@gmail.com>
2022-03-21 22:50:59 -0300

    Animation Logo Splash Screen Done

--- app/src/main/java/com/martin/preventapp/SplashScreen.java
+++ app/src/main/java/com/martin/preventapp/SplashScreen.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.martin.preventapp.firebase.login.LoginActivity;
@@ -12,11 +15,21 @@ import com.martin.preventapp.firebase.login.LoginActivity;
 public class SplashScreen extends Activity {
 
     int SPLASH_TIME = 1000; //This is 3 seconds
+    Animation topAnim;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.splash_screen);
+
+        //Hooks
+        image = findViewById(R.id.ImageViewLogoLinkiar);
+
+        image.setAnimation(topAnim);
 
         //Code to start timer and take action after the timer ends
         new Handler(getMainLooper()).postDelayed(new Runnable() {

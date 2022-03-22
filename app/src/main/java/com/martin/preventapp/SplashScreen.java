3154d497c946f290c49141bf1b5a6f138589036a
Martin Ballester <martinballester1@gmail.com>
2022-03-21 22:31:20 -0300

    work on Splash Screen

--- app/src/main/java/com/martin/preventapp/SplashScreen.java
+++ app/src/main/java/com/martin/preventapp/SplashScreen.java
@@ -1,14 +1,35 @@
 package com.martin.preventapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
-public class SplashScreen extends AppCompatActivity {
+import com.martin.preventapp.firebase.login.LoginActivity;
+
+public class SplashScreen extends Activity {
+
+    int SPLASH_TIME = 1000; //This is 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Code to start timer and take action after the timer ends
+        new Handler(getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(mySuperIntent);
+
+                //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+                finish();
+
+            }
+        }, SPLASH_TIME);
     }
 }

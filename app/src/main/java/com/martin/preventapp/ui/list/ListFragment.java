ae90e824dfcf22ccdea72f65798e5c4174c363ac
Martin Ballester <martinballester1@gmail.com>
2022-03-28 11:04:16 -0300

    file picker work with .xls

--- app/src/main/java/com/martin/preventapp/ui/list/ListFragment.java
+++ app/src/main/java/com/martin/preventapp/ui/list/ListFragment.java
@@ -1,18 +1,55 @@
 package com.martin.preventapp.ui.list;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.martin.preventapp.R;
 import com.martin.preventapp.databinding.FragmentListBinding;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbookFactory;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Header;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 
 public class ListFragment extends Fragment {
 
@@ -31,12 +68,73 @@ public class ListFragment extends Fragment {
         Button listFile = root.findViewById(R.id.list_file);
         Button clientsFile = root.findViewById(R.id.client_file);
 
+        //file chooser
+
+        listFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileDialog(root);
+            }
+        });
+
+
+
         //TV test
         TextView tvTest = root.findViewById(R.id.textView6);
 
-        
+        int EXTERNAL_STORAGE_PERMISSION_CODE = 23;
+
+        // Requesting Permission to access External Storage
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                EXTERNAL_STORAGE_PERMISSION_CODE);
+
+        //"sdcard/Download/contacts.xlsx"
+
+        File file = new File(getActivity().getExternalFilesDir(null), "contacts.xls");
+        FileInputStream inputStream = null;
 
 
+        try {
+
+            //WORK WITH .XLS FILES
+
+            inputStream = new FileInputStream(file);
+
+            POIFSFileSystem fileSystem = new POIFSFileSystem(inputStream);
+
+            HSSFWorkbook workbook = new HSSFWorkbook(fileSystem);
+
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = workbook.getSheetAt(0);
+            // We now need something to iterate through the cells.
+            Iterator<Row> rowIter = mySheet.rowIterator();
+            int rowno =0;
+            tvTest.setText("\n");
+            while (rowIter.hasNext()) {
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                if (rowno != 0) {
+                    Iterator<Cell> cellIter = myRow.cellIterator();
+                    int colno = 0;
+                    String sno = "", date = "", det = "";
+                    while (cellIter.hasNext()) {
+                        HSSFCell myCell = (HSSFCell) cellIter.next();
+                        if (colno == 0) {
+                            sno = myCell.toString();
+                        } else if (colno == 1) {
+                            date = myCell.toString();
+                        } else if (colno == 2) {
+                            det = myCell.toString();
+                        }
+                        colno++;
+                    }
+                    tvTest.append(sno + " -- " + date + "  -- " + det + "\n\n");
+                }
+                rowno++;
+            }
+
+            } catch (Exception e) {
+                Toast.makeText(root.getContext(), "error: " + e.toString(), Toast.LENGTH_SHORT).show();
+        }
 
         return root;
     }
@@ -46,4 +144,27 @@ public class ListFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public void openFileDialog(View view){
+        Intent data = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        data.setType("application/vnd.ms-excel");
+        data = Intent.createChooser(data, "Choose a file");
+        sActivityResultLauncher.launch(data);
+    }
+
+    ActivityResultLauncher<Intent> sActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        Uri uri = data.getData();
+                        Toast.makeText(getContext(), uri.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+
 }
